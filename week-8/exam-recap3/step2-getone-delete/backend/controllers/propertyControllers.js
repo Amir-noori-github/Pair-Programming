30cc85bf3cf76b7d2e69feb7b1c775fdf9655081
Amir Noori <amir.noori@metropolia.fi>
2025-10-06 22:22:32 +0300

    Reading and Deleting a Single property also API tests Done

---
+++ week-8/exam-recap3/step2-getone-delete/backend/controllers/propertyControllers.js
@@ -0,0 +1,78 @@
+const Property = require("../models/propertyModel");
+const mongoose = require("mongoose");
+
+
+// GET /properties
+const getAllProperties = async (req, res) => {
+  try {
+    const properties = await Property.find({}).sort({ createdAt: -1 });
+    res.status(200).json(properties);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve properties" });
+  }
+};
+
+// POST /properties
+const createProperty = async (req, res) => {
+  try {
+    console.log("Incoming property:", req.body);
+
+    const newProperty = await Property.create(req.body);
+    res.status(201).json(newProperty);
+  } catch (error) {
+    res.status(400).json({ message: "Failed to create property", error: error.message });
+  }
+};
+
+// GET /properties/:propertyId
+const getPropertyById = async (req, res) => {
+  const { propertyId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(propertyId)) {
+    return res.status(400).json({ message: "Invalid property ID" });
+  }
+
+  try {
+    const property = await Property.findById(propertyId);
+    if (property) {
+      res.status(200).json(property);
+    } else {
+      res.status(404).json({ message: "Property not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve property" });
+  }
+};
+
+// PUT /properties/:propertyId
+const updateProperty = async (req, res) => {
+  res.send("updateProperty");
+};
+
+// DELETE /properties/:propertyId
+const deleteProperty = async (req, res) => {
+  const { propertyId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(propertyId)) {
+    return res.status(400).json({ message: "Invalid property ID" });
+  }
+
+  try {
+    const deletedProperty = await Property.findOneAndDelete({ _id: propertyId });
+    if (deletedProperty) {
+      res.status(204).send(); // 204 No Content
+    } else {
+      res.status(404).json({ message: "Property not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete property" });
+  }
+};
+
+module.exports = {
+  getAllProperties,
+   createProperty,
+  getPropertyById,
+  // updateProperty,
+  deleteProperty,
+};
