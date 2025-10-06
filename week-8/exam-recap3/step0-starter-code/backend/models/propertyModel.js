c90df8a9002e8f90a42b90adab27cb88283e8ade
Amir Noori <amir.noori@metropolia.fi>
2025-10-06 22:10:54 +0300

    Create starter-code for Property Model

---
+++ week-8/exam-recap3/step0-starter-code/backend/models/propertyModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const propertySchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  type: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: {
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipCode: { type: String, required: true }
+  },
+  squareFeet: { type: Number, required: true },
+  yearBuilt: { type: Number, required: true }
+});
+
+// Add virtual field `id`
+propertySchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    return ret;
+  }
+});
+
+const Property = mongoose.model('Property', propertySchema);
+
+module.exports = Property;
